43d2ec1ed1bebde8b4fd6c00cd6e29589b17fa0d
annndro <anndrozd1010@gmail.com>
2021-11-03 20:22:21 +0000

    second version - simplified

--- script.js
+++ script.js
@@ -3,16 +3,10 @@ alert("Hello Visitor, welcome to my page! Please complete the contact form and I
 }
 function enquiry(){
 	event.preventDefault();
-	var visitorName=document.getElementById("vname").value;
-	var visitorEmail=document.getElementById("vemail").value;
-	hideform();
-}
-function hideform(){
 	document.getElementById("contact_form").style.display="none";
-	outputMsg();
-}
-function outputMsg(){
+	
 	var visitorName=document.getElementById("vname").value;
 	var visitorEmail=document.getElementById("vemail").value;
+	
 	alert(visitorName+" thank you for your details. We will be in touch via "+visitorEmail+" shortly.");
 }
